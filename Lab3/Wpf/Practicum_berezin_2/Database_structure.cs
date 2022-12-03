6d0cd51c92eed85dda349ea749820eafe5040989
Gleb Nazarenko <90349792+Fedya2006@users.noreply.github.com>
2022-12-03 15:42:01 +0300

    Лабораторная #3

---
+++ Lab3/Wpf/Practicum_berezin_2/Database_structure.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore;
+
+namespace Practicum_berezin_2
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Hash { get; set; }
+        public Image_directly Blob { get; set; }
+        public List<Emotion> Emotions { get; set; }
+
+        public static string GetHash(byte[] data)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                return string.Concat(sha256.ComputeHash(data).Select(x => x.ToString("X2")));
+            }
+        }
+
+    }
+
+    public class Image_directly
+    {
+        public int Id { get; set; }
+        public Image image { get; set; }
+        public byte[] Raw_data { get; set; }
+        public int ImageId { get; set; }
+    }
+
+    public class Emotion
+    {
+        public int Id { get; set; } 
+        public string emotion_name { get; set; }
+        public float emotion_val { get; set; }
+        public int ImageId { get; set; } 
+        public Image image { get; set; }
+
+    }
+
+
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Image_directly> Blobs { get; set; }
+        public DbSet<Emotion> Image_emotions { get; set; } 
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder o)
+        {
+            o.UseSqlite("Data Source=images_db.db");
+        }
+    }
+}
---
+++ Lab3/Wpf/Practicum_berezin_2/Migrations/20221202123659_Init.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Practicumberezin2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Hash = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Blobs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ImageId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Rawdata = table.Column<byte[]>(name: "Raw_data", type: "BLOB", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blobs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Blobs_Images_ImageId",
+                        column: x => x.ImageId,
+                        principalTable: "Images",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Image_emotions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    emotionname = table.Column<string>(name: "emotion_name", type: "TEXT", nullable: false),
+                    emotionval = table.Column<float>(name: "emotion_val", type: "REAL", nullable: false),
+                    ImageId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Image_emotions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Image_emotions_Images_ImageId",
+                        column: x => x.ImageId,
+                        principalTable: "Images",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blobs_ImageId",
+                table: "Blobs",
+                column: "ImageId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Image_emotions_ImageId",
+                table: "Image_emotions",
+                column: "ImageId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blobs");
+
+            migrationBuilder.DropTable(
+                name: "Image_emotions");
+
+            migrationBuilder.DropTable(
+                name: "Images");
+        }
+    }
+}
